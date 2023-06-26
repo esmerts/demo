35eebdce737e7411c7f509fb57b8e12b43b88a3b
@liu-xun2001 <2449617459@qq.com>
2023-06-26 23:51:40 +0800

    设置打包后本地运行的源路径

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/home'
+  // publicPath: 'http://127.0.0.1:57450/demo/dist/', // 生产环境下，打包后在本地运行
+  // publicPath: '/home', // 开发环境下，npm run serve后运行
+  publicPath: process.env.NODE_ENV === 'production' ? 'http://127.0.0.1:57450/demo/dist/' : '/home'
 })
